1c317dec2b4796b450f145eb214451ac75d71730
RoranovaZoro <kashdhee@gmail.com>
2023-08-26 15:53:22 +0530

    Attempt1

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -9,17 +9,29 @@ public class Car extends Vehicle {
     private int currentGear;
     private int seats;
 
-    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        //Hint: Car extends Vehicle
+    public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats)
+    {
+        super(name);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManual = isManual;
+        this.type = type;
+        this.seats = seats;
+        this.currentGear = 1;
     }
 
-    public void changeGear(int newGear){
-
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+    public void changeGear(int newGear) {
+        if (newGear >= 1 && newGear <= gears) {
+            currentGear = newGear;
+            System.out.println("Gear changed to: " + currentGear);
+        } else {
+            System.out.println("Invalid gear value");
+        }
     }
 
-    public void changeSpeed(int newSpeed, int newDirection){
-
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    public void changeSpeed(int newSpeed, int newDirection) {
+        move(newSpeed, newDirection);
     }
 }
+
